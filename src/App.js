78d2b10f702c1098239cc4f08154650181b979f4
nidhikowtal <kowtalnidhi@gmail.com>
2023-01-30 12:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBTextArea,
+}
+from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBContainer fluid>
+
+      <MDBRow className='d-flex justify-content-center align-items-center'>
+        <MDBCol lg='9' className='my-5'>
+
+          <h1 class="text-white mb-4">Enter the Jira Story</h1>
+
+          <MDBCard>
+            <MDBCardBody className='px-4'>
+              <MDBRow className='align-items-center pt-4 pb-3'>
+
+                <MDBCol md='3' className='ps-5'>
+                  <h6 className="mb-0"></h6>
+                </MDBCol>
+
+                <MDBCol md='9' className='pe-5'>
+                  <MDBTextArea id='textAreaExample' rows={3} />
+                </MDBCol>
+
+              </MDBRow>
+
+
+              <button type="submit">
+            Submit
+          </button>
+
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
   );
 }
 
 export default App;
+
+
+
+
